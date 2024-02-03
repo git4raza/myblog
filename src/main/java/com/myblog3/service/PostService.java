85a0a0fa7dabb2d3ac1eea6b338ce07bf50ed2ef
Raza <mdrazaullah95045@gmail.com>
2024-02-03 12:09:51 +0530

    101:Create Save Post Feature Updated on 09/01/2023

--- src/main/java/com/myblog3/service/PostService.java
+++ src/main/java/com/myblog3/service/PostService.java
@@ -2,6 +2,17 @@ package com.myblog3.service;
 
 import com.myblog3.payload.PostDto;
 
+import java.util.List;
+
 public interface PostService {
     PostDto createPost(PostDto postDto);
+
+    PostDto getPostById(long id);
+
+
+    List<PostDto> getAllPosts(int pageNo, int pageSize);
+
+    void deletePost(long id);
+
+    PostDto updatePost(long raza, PostDto postDto);
 }
